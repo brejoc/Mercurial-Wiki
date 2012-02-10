4fed3fcca232b755c8eb7e101acb5f10070ca3f8
Jochen Breuer <brejoc@gmail.com>
2012-02-10 12:14:03 +0100

    Added version.

--- hgwiki.py
+++ hgwiki.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__version__ = '1.1'
+
 from __future__ import with_statement
 
 import os
